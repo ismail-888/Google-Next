f6590f6cecb7e115a6152bb04115eb6feeca5009
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-10-17 13:16:56 +0300

    create search page for web and image

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,12 @@
+import SearchHeader from "@/components/SearchHeader";
+import "./../globals.css"
+const layout = ({ children }) => {
+  return (
+    <div>
+      <SearchHeader />
+      {children}
+    </div>
+  );
+};
+
+export default layout;
