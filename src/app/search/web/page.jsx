3611e1b68ac8a0f4a97d4ec29a040c20e4f7b25b
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-10-18 05:21:10 +0300

    try to deploy

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,25 +1,32 @@
+// search/image/page.js
 import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link';
+import { Suspense } from 'react';
+
+function Loading() {
+  return <div>Loading...</div>;
+}
 
 export default async function WebSearchPage({ searchParams }) {
   const startIndex = searchParams.start || '1';
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
   );
+
   if (!response.ok) throw new Error('Something went wrong');
   const data = await response.json();
   const results = data.items;
 
   if (!results) {
     return (
-      <div className='flex flex-col justify-center items-center pt-10'>
-        <h1 className='text-3xl mb-4'>
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
           No results found for {searchParams.searchTerm}
         </h1>
-        <p className='text-lg'>
+        <p className="text-lg">
           Try searching the web or images for something else{' '}
-          <Link href='/' className='text-blue-500'>
+          <Link href="/" className="text-blue-500">
             Home
           </Link>
         </p>
@@ -27,5 +34,9 @@ export default async function WebSearchPage({ searchParams }) {
     );
   }
 
-  return <div>{results && <WebSearchResults results={data} />}</div>;
+  return (
+    <Suspense fallback={<Loading />}>
+      <div>{results && <WebSearchResults results={data} />}</div>
+    </Suspense>
+  );
 }
