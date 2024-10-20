862ea96b2794a1cfb3797fcff947a5526e461e42
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-10-20 15:23:25 +0300

    b

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,30 +1,27 @@
-// app/search/web/page.jsx
-import WebSearchClient from '@/components/WebSearchClient';
+import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link';
 
 export default async function WebSearchPage({ searchParams }) {
-  const searchTerm = searchParams.searchTerm || ''; // Get the search term from the query
   const startIndex = searchParams.start || '1';
-
-  if (!searchTerm) {
-    return <p>Please provide a search term.</p>;
-  }
-
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}&start=${startIndex}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
   );
-
   if (!response.ok) throw new Error('Something went wrong');
   const data = await response.json();
+  console.log(data)
   const results = data.items;
+
 
   if (!results) {
     return (
-      <div className="flex flex-col justify-center items-center pt-10">
-        <h1 className="text-3xl mb-4">No results found for {searchTerm}</h1>
-        <p className="text-lg">
-          Try searching the web{' '}
-          <Link href="/" className="text-blue-500">
+      <div className='flex flex-col justify-center items-center pt-10'>
+        <h1 className='text-3xl mb-4'>
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className='text-lg'>
+          Try searching the web or images for something else{' '}
+          <Link href='/' className='text-blue-500'>
             Home
           </Link>
         </p>
@@ -32,5 +29,5 @@ export default async function WebSearchPage({ searchParams }) {
     );
   }
 
-  return <WebSearchClient results={results} />;
+  return <div>{results && <WebSearchResults results={data} />}</div>;
 }
