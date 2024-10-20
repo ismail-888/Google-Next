f3746c21391becd95c42a72f270397bfc9a65de7
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-10-20 14:25:34 +0300

    a

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,17 @@
+// app/search/web/page.jsx
 import WebSearchClient from '@/components/WebSearchClient';
 import Link from 'next/link';
 
-export default async function WebSearchPage() {
+export default async function WebSearchPage({ searchParams }) {
+  const searchTerm = searchParams.searchTerm || ''; // Get the search term from the query
+  const startIndex = searchParams.start || '1';
+
+  if (!searchTerm) {
+    return <p>Please provide a search term.</p>;
+  }
+
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=yourQueryHere&start=1`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}&start=${startIndex}`
   );
 
   if (!response.ok) throw new Error('Something went wrong');
@@ -13,7 +21,7 @@ export default async function WebSearchPage() {
   if (!results) {
     return (
       <div className="flex flex-col justify-center items-center pt-10">
-        <h1 className="text-3xl mb-4">No results found</h1>
+        <h1 className="text-3xl mb-4">No results found for {searchTerm}</h1>
         <p className="text-lg">
           Try searching the web{' '}
           <Link href="/" className="text-blue-500">
